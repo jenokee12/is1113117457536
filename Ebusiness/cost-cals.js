5e3684f97aaa65845dcb7bbbbe1fa0445ba44249
Jennifer O'Keeffe <117457536@umail.ucc.ie>
2018-03-06 16:35:43 +0000

    uptodate

--- Ebusiness/cost-cals.js
+++ Ebusiness/cost-cals.js
@@ -1,40 +1,63 @@
 /* global $ */
 
-
 function calcSub(){
-
+    
     var argSubTotal;
-
+    
     if(document.getElementById('salesforce').checked) {
-
       argSubTotal = 100;
-
     }
-    else {
+    else if (document.getElementById('cloud9').checked){
+      argSubTotal = 200;
+    }
+    else if (document.getElementById('aws').checked){
       argSubTotal = 300;
     }
-
-    display(argSubTotal);
+    else{
+      argSubTotal = 400;
+    }
+    
+    calcDisVatTotal(argSubTotal);
 }
 
 
-function display(parm1){
+function calcDisVatTotal(parmSubTotal){
+  var subTotal = parmSubTotal;
+  var discountAmt;
+  var vatAmt;
+  var totalPrice;
 
+  discountAmt = (parmSubTotal * 0.05);
+  
+  vatAmt = ((parmSubTotal - discountAmt) * 0.1);
+  
+  totalPrice = ((parmSubTotal + vatAmt) - discountAmt);
+  
+  display(subTotal, discountAmt, vatAmt, totalPrice);
+}
+
+
+function display(parm1, parm2, parm3, parm4){
+  
   document.getElementById("subtotal").value = parm1;
-
-  document.getElementById("total").value = parm1;
-
+  document.getElementById("discount").value = parm2;
+  document.getElementById("vat").value = parm3;
+  document.getElementById("total").value = parm4;
+        
   enablebtnProceed();
 }
 
 
 function enablebtnProceed(){
-
     $('#btnProceed').prop('disabled', false);
 }
 
-
 function disablebtnProceed() {
-
     $('#btnProceed').prop('disabled', true);
+}
+
+
+function rbChecked(){
+  disablebtnProceed();
+    
 }
